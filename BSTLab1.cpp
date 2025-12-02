e3a6a4ca6621e2d7f941db50ac573c005cbd1f81
JackGz32 <jacklyn.gonzalez02@utrgv.edu>
2025-12-02 00:05:19 +0000

    working on search func

--- BSTLab1.cpp
+++ BSTLab1.cpp
@@ -8,12 +8,20 @@ class Node {
 		// the Node should contain
 		// 	- an integer "key" that holds the value of the Node
 		// 	- a pointer to each child possible child node ("right" and "left")
+		int key;
+		Node* left;
+		Node* right; 
+
 
 
 		// Write a constructor for the Node
 		// input: only an integer value for "key", no pointer arguments
 		// 		right and left pointers should be null
-
+		Node(int value) {
+			key = value;
+			left = nullptr;
+			right = nullptr;	
+		}
 };
 
 
@@ -21,23 +29,39 @@ class Node {
 // {13, 0, 7, 6, 21, 15, -2, 12, 99, 18, 19, -1}
 Node* createTree() {
 	// root
-	Node *root = nullptr;
+	Node *root = 13;
 
 	// level 1 (children of root)
+	root->left = 0:
+	root->right = 21;
 
 	// level 2 (children of 0)
-
+	root->left->left = -2;
+	root->left->right = 7;
+	
 	// level 2 (children of 21)
+	root->right->left = 15;
+	root->right->right = 99;
 
 	// level 3 (children of -2)
+	root->left->left->left = -1;
+	root->left->left->right = nullptr;
 
 	// level 3 (children of 7)
+	root->left->right->left = 6;
+	root->left->right->right = 12;
 
 	// level 3 (children of 15)
+	root->right->left->left = nullptr;
+	root->right->left->right = 18;
 
 	// level 3 (children of 99)
+	root->right->right->left = nullptr;
+	root->right->right->right = nullptr;
 
 	// level 4 (children of 18)
+	root->right->left->right->left = nullptr;
+	root->right->left->right->right = 19;
 
 	return root;
 }
@@ -49,8 +73,17 @@ Node* createTree() {
 // You should write this function recursively! What is the base case? What is the general case?
 bool searchTree(int target, Node* root) {
 	// Base cases
-	return false;
-
+	Node* current = root;
+	while (current != nullptr) {
+		if (current ->key == value){
+			return true;
+		}
+		if (value < current->key) {
+			current = current->left;
+		} else {
+			current = current->right;
+		}
+	}
 	// General case
 	return false;
 	
